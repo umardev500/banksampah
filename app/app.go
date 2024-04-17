fc50f0af57247de2338dcd34a28fb1c15f2da26e
Umar <umar.dev500@gmail.com>
2024-04-17 07:35:30 +0700

    initial commit

---
+++ app/app.go
@@ -0,0 +1,40 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/rs/zerolog/log"
+)
+
+type App struct{}
+
+func New() *App {
+	return &App{}
+}
+
+func (app *App) Run(ctx context.Context) error {
+	fiberApp := fiber.New()
+	ch := make(chan error, 1)
+	go func() {
+		port := os.Getenv("PORT")
+		addr := ":" + port
+
+		log.Info().Msgf("Listening on %s 🔥", port)
+		ch <- fiberApp.Listen(addr, fiber.ListenConfig{
+			DisableStartupMessage: true,
+		})
+		close(ch)
+	}()
+
+	select {
+	case err := <-ch:
+		return err
+	case <-ctx.Done():
+		fmt.Println() // add new line
+		log.Info().Msgf("Gracefully shutting down... 😪")
+		return fiberApp.Shutdown()
+	}
+}
---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/umardev500/banksampah/app"
+)
+
+func init() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	if err := godotenv.Load(); err != nil {
+		log.Fatal().Msgf("error loading .env file: %v", err)
+	}
+}
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Kill, os.Interrupt)
+	defer cancel()
+
+	application := app.New()
+	err := application.Run(ctx)
+	if err != nil {
+		log.Fatal().Msgf("error running app: %v", err)
+	}
+
+}

be3d103ca2974dfb027468d2f3a895e8567125b1
Umar <umar.dev500@gmail.com>
2024-04-17 12:45:24 +0700

    add user and create user model

--- domain/model/user_model.go
+++ domain/model/user_model.go
@@ -1,3 +1,23 @@
 package model
 
-type User struct{}
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID        primitive.ObjectID `json:"id" bson:"_id"`
+	Email     string             `json:"email"`
+	Username  string             `json:"username"`
+	Password  string             `json:"password"`
+	CreatedAt time.Time          `json:"created_at"`
+	UpdatedAt *time.Time         `json:"updated_at,omitempty"`
+	DeletedAt *time.Time         `json:"deleted_at,omitempty"`
+}
+
+type CreateUser struct {
+	Email    string `json:"email" validate:"required,min=8"`
+	Username string `json:"username" validate:"required,min=8"`
+	Password string `json:"password" validate:"required,min=8"`
+}

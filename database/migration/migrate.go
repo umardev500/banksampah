93238134c9314ad5fa6ff2db36d14abf63b4dfe4
Umar <umar.dev500@gmail.com>
2024-04-22 06:44:10 +0700

    add migration

---
+++ database/migration/migrate.go
@@ -0,0 +1,54 @@
+package migration
+
+import (
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/rs/zerolog/log"
+	"github.com/umardev500/banksampah/config"
+)
+
+type Migration struct {
+	M *migrate.Migrate
+}
+
+func NewMigrate() *Migration {
+	conn := config.NewPostgress()
+	driver, err := postgres.WithInstance(conn.DBRaw, &postgres.Config{})
+	if err != nil {
+		log.Fatal().Msgf("error driver connecting to postgres: %v", err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://database/migration/files",
+		"postgres",
+		driver,
+	)
+
+	if err != nil {
+		log.Fatal().Msgf("error creating new migrate instance: %v", err)
+	}
+
+	log.Info().Msgf("🎉 Migration ready!")
+
+	return &Migration{
+		M: m,
+	}
+}
+
+func (m *Migration) Up() {
+	log.Info().Msgf("📦 Migrating up...")
+	if err := m.M.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal().Msgf("error migrating up: %v", err)
+	}
+	log.Info().Msgf("📦 Migrated up successfully!")
+}
+
+func (m *Migration) Down() {
+	log.Info().Msgf("📦 Migrating down...")
+	if err := m.M.Down(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal().Msgf("error migrating down: %v", err)
+	}
+	log.Info().Msgf("📦 Migrated down successfully!")
+}
--- go.mod
+++ go.mod
@@ -5,10 +5,12 @@ go 1.22.2
 require (
 	github.com/go-playground/validator/v10 v10.19.0
 	github.com/gofiber/fiber/v3 v3.0.0-beta.2
+	github.com/golang-migrate/migrate v3.5.4+incompatible
+	github.com/golang-migrate/migrate/v4 v4.17.1
 	github.com/google/uuid v1.6.0
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/joho/godotenv v1.5.1
-	github.com/lib/pq v1.2.0
+	github.com/lib/pq v1.10.9
 	github.com/rs/zerolog v1.32.0
 )
 
@@ -18,6 +20,8 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/gofiber/utils/v2 v2.0.0-beta.4 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/klauspost/compress v1.17.8 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -25,7 +29,8 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.52.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/crypto v0.19.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/crypto v0.20.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect

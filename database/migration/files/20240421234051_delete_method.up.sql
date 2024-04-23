28c8847f90be0d79a6e652bef67a1ac86463fc37
Umar <umar.dev500@gmail.com>
2024-04-24 05:46:11 +0700

    change migration of delete_method

--- database/migration/files/20240421234051_delete_method.up.sql
+++ database/migration/files/20240421234051_delete_method.up.sql
@@ -1,24 +1,18 @@
 CREATE OR REPLACE FUNCTION delete_record(
     tbl VARCHAR(10),
     row_id UUID,
-    delete_type VARCHAR(10)
+    is_soft_delete BOOLEAN = FALSE
 ) RETURNS INT AS $$
 DECLARE
     sql_query TEXT;
     rows_affected INT;
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = tbl) THEN
-        RAISE EXCEPTION 'Table % does not exist.', tbl;
-    END IF;
-
     sql_query := '';
     
-    IF delete_type = 'soft' THEN
+    IF is_soft_delete THEN
         sql_query := 'UPDATE ' || tbl || ' SET deleted_at = CURRENT_TIMESTAMP WHERE id = $1';
-    ELSIF delete_type = 'hard' THEN
-        sql_query := 'DELETE FROM ' || tbl || ' WHERE id = $1';
     ELSE
-        RAISE EXCEPTION 'Invalid delete type. Please specify either ''soft'' or ''hard''.';
+        sql_query := 'DELETE FROM ' || tbl || ' WHERE id = $1';
     END IF;
 
     EXECUTE sql_query USING row_id;

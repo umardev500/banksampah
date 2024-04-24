ad9142feeccb4cf690c7b5a09bdbb9f0afc2d943
Umar <umar.dev500@gmail.com>
2024-04-24 07:22:02 +0700

    add pgx conf

---
+++ config/pgx.go
@@ -0,0 +1,85 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rs/zerolog/log"
+	"github.com/umardev500/banksampah/constant"
+)
+
+type PgxQuery interface {
+	CopyFrom(ctx context.Context, tableName pgx.Identifier, columnNames []string, rowSrc pgx.CopyFromSource) (int64, error)
+	SendBatch(ctx context.Context, b *pgx.Batch) pgx.BatchResults
+
+	Exec(ctx context.Context, sql string, arguments ...any) (commandTag pgconn.CommandTag, err error)
+	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
+}
+
+type PgxConfig struct {
+	Pool  *pgxpool.Pool
+	Query PgxQuery
+}
+
+func NewPgx() *PgxConfig {
+	log.Info().Msg("🐘 Connecting to Postgres...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conf := &PgxConfig{}
+
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DATABASE")
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	dbpool, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		log.Fatal().Msgf("error connecting to postgres: %v", err)
+	}
+
+	log.Info().Msg("🤝 Postgres ping....")
+
+	if err := dbpool.Ping(ctx); err != nil {
+		log.Fatal().Msgf("error pinging postgres: %v", err)
+	}
+
+	log.Info().Msg("👋 Postgres connected successfully!")
+	conf.Pool = dbpool
+
+	return conf
+}
+
+func (conf *PgxConfig) WithTransaction(ctx context.Context, fn func(ctx context.Context) error) error {
+	tx, err := conf.Pool.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback(ctx) // Rollback transaction if not committed
+
+	ctx = context.WithValue(ctx, constant.Tx, tx)
+	err = fn(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Get connection type from context
+func (conf *PgxConfig) TrOrDB(ctx context.Context) PgxQuery {
+	if tx, ok := ctx.Value(constant.Tx).(PgxQuery); ok {
+		log.Debug().Msgf("💪 Connection type is transaction ")
+		return tx
+	}
+
+	return conf.Pool
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gofiber/fiber/v3 v3.0.0-beta.2
 	github.com/golang-migrate/migrate/v4 v4.17.1
 	github.com/google/uuid v1.6.0
+	github.com/jackc/pgx/v5 v5.5.5
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
@@ -21,6 +22,9 @@ require (
 	github.com/gofiber/utils/v2 v2.0.0-beta.4 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/klauspost/compress v1.17.8 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -31,6 +35,7 @@ require (
 	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/crypto v0.20.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )

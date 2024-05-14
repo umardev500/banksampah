b34e11ce9c49e4e98b7c16eedcdec4392a2ca1ef
Umar <umar.dev500@gmail.com>
2024-05-15 06:47:55 +0700

    add permission schema

---
+++ database/migration/files/20240514225011_permissions.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS permissions (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "name" VARCHAR(50) NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT NULL,
+    deleted_at TIMESTAMPTZ DEFAULT NULL
+);

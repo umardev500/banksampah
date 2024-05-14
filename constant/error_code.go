7d106b13f2715a12594b5cb70f13e260c3eeb335
Umar <umar.dev500@gmail.com>
2024-05-14 21:36:15 +0700

    refactor validator

--- constant/error_code.go
+++ constant/error_code.go
@@ -3,5 +3,6 @@ package constant
 type ErrCodeName string
 
 const (
-	ErrCodeNameDuplicate ErrCodeName = "DUPLICATE"
+	ErrCodeNameDuplicate  ErrCodeName = "DUPLICATE"
+	ErrCodeNameValidation ErrCodeName = "VALIDATION"
 )
--- util/validator.go
+++ util/validator.go
@@ -6,38 +6,43 @@ import (
 
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v3"
+	"github.com/google/uuid"
+	"github.com/umardev500/banksampah/constant"
 )
 
 type ValidationDetail struct {
-	Tag    string `json:"tag"`
-	Detail string `json:"detail"`
-}
-
-type ValidationResponse struct {
-	Message string                      `json:"message"`
-	Errors  map[string]ValidationDetail `json:"errors"`
+	Field string `json:"field"`
+	Tag   string `json:"tag"`
+	Error string `json:"error"`
 }
 
 func ValidateJson(c fiber.Ctx, v *validator.Validate, payload interface{}) (error, error) {
-	errs := make(map[string]ValidationDetail)
+	// errs := make(map[string]ValidationDetail)
+	var errs []ValidationDetail
 
 	if err := v.Struct(payload); err != nil {
+		ticket := uuid.New()
 		if validationErrors, ok := err.(validator.ValidationErrors); ok {
 			for _, validationErr := range validationErrors {
 				field := strings.ToLower(validationErr.Field())
 				tag := validationErr.Tag()
 
-				var detail string = validationMapping(validationErr)
-				errs[field] = ValidationDetail{
-					Tag:    tag,
-					Detail: detail,
-				}
+				var errMsg string = validationMapping(validationErr)
+				errs = append(errs, ValidationDetail{
+					Field: field,
+					Tag:   tag,
+					Error: errMsg,
+				})
 			}
 		}
 
-		resp := ValidationResponse{
+		resp := Response{
+			Ticket:  ticket,
 			Message: "Validation error",
-			Errors:  errs,
+			Error: &ResponseError{
+				Code:    constant.ErrCodeNameValidation,
+				Details: errs,
+			},
 		}
 
 		return c.Status(fiber.StatusBadRequest).JSON(resp), err
@@ -54,13 +59,13 @@ func validationMapping(validationErr validator.FieldError) string {
 
 	switch tag {
 	case "required":
-		detail = fmt.Sprintf("Field '%s' is required", field)
+		detail = fmt.Sprintf("%s is required.", field)
 	case "min":
-		detail = fmt.Sprintf("Field '%s' must be at least %s characters long", field, validationErr.Param())
+		detail = fmt.Sprintf("%s must be at least %s characters long.", field, validationErr.Param())
 	case "max":
-		detail = fmt.Sprintf("Field '%s' must be at most %s characters long", field, validationErr.Param())
+		detail = fmt.Sprintf("%s must be at most %s characters long.", field, validationErr.Param())
 	case "email":
-		detail = fmt.Sprintf("Field '%s' must be a valid email address", field)
+		detail = fmt.Sprintf("%s must be a valid email address.", field)
 		// Add more cases for other validation tags as needed
 	default:
 		detail = fmt.Sprintf("Field %s is %s", field, validationErr.Error())

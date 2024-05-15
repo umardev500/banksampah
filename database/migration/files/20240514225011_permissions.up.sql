34dd74c77e0320c94bae03bc29889efcdc8a5fa4
Umar <umar.dev500@gmail.com>
2024-05-15 07:56:46 +0700

    add feature_id field to permissions

--- database/migration/files/20240514225011_permissions.up.sql
+++ database/migration/files/20240514225011_permissions.up.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS permissions (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    feature_id UUID NOT NULL,
     "name" VARCHAR(50) NOT NULL,
     "description" VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ DEFAULT NULL,
-    deleted_at TIMESTAMPTZ DEFAULT NULL
+    deleted_at TIMESTAMPTZ DEFAULT NULL,
+    FOREIGN KEY (feature_id) REFERENCES features(id)
 );

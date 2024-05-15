e2d290a8bf81b98df20daef41c6169f8a346bd94
Umar <umar.dev500@gmail.com>
2024-05-15 10:29:12 +0700

    add status

--- database/migration/files/20240514225009_features.up.sql
+++ database/migration/files/20240514225009_features.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS features (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     "name" VARCHAR(50) UNIQUE NOT NULL,
     "description" VARCHAR(255) NOT NULL,
+    "status" VARCHAR(25) DEFAULT 'active' CHECK ("status" IN ('active', 'inactive', 'always_active')),
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ DEFAULT NULL,
     deleted_at TIMESTAMPTZ DEFAULT NULL

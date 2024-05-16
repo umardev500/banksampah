e53ffce001d5dd4b32b309a187c169ad2ed36733
Umar <umar.dev500@gmail.com>
2024-05-16 10:10:54 +0700

    add find types:

---
+++ types/find.go
@@ -0,0 +1,24 @@
+package types
+
+type Pagination struct {
+	Page   int `json:"page"`
+	Limit  int `json:"limit"`
+	Offset int `json:"offset"`
+	Total  int `json:"total"`
+}
+
+type Filter struct {
+	Field string      `json:"field"`
+	Value interface{} `json:"value"`
+}
+
+type Order struct {
+	Field string `json:"field"`
+	Dir   string `json:"dir"` // ASC or DESC
+}
+
+type QueryParam struct {
+	Pagination Pagination `json:"pagination"`
+	Filter     []Filter   `json:"filter"`
+	Order      Order      `json:"order"`
+}

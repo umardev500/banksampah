2d2289f398be04241bc807bb4dc9c454a7478b4d
Umar <umar.dev500@gmail.com>
2024-05-16 23:15:07 +0700

    define interface

--- domain/waste_type_domain.go
+++ domain/waste_type_domain.go
@@ -17,6 +17,7 @@ type WasteTypeHandler interface {
 type WasteTypeUsecase interface {
 	Find(ctx context.Context, params *types.QueryParam) util.Response
 	DeleteByID(ctx context.Context, id string) util.Response
+	UpdateByID(ctx context.Context, id string) util.Response
 }
 
 type WasteTypeRepository interface {

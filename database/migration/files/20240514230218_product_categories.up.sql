e38b0441539320b405bf1775df6435bf265c5026
Umar <umar.dev500@gmail.com>
2024-05-16 08:35:31 +0700

    add description field

--- database/migration/files/20240514230218_product_categories.up.sql
+++ database/migration/files/20240514230218_product_categories.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS product_categories (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     "name" VARCHAR(25) UNIQUE,
     "point" DECIMAL(7, 2),
+    "description" TEXT,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ DEFAULT NULL,
     deleted_at TIMESTAMPTZ DEFAULT NULL

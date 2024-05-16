f33931fc7d00d01f49cc8540978d96c4558c829b
Umar <umar.dev500@gmail.com>
2024-05-16 09:50:03 +0700

    add ansi constant

---
+++ constant/ansi.go
@@ -0,0 +1,14 @@
+package constant
+
+const (
+	ANSI_BLACK   = "\033[1;30m"
+	ANSI_RED     = "\033[1;31m"
+	ANSI_DARKRED = "\033[0;31m"
+	ANSI_GREEN   = "\033[1;32m"
+	ANSI_YELLOW  = "\033[1;33m"
+	ANSI_BLUE    = "\033[1;34m"
+	ANSI_PURPLE  = "\033[1;35m"
+	ANSI_CYAN    = "\033[1;36m"
+	ANSI_WHITE   = "\033[1;37m"
+	ANSI_RESET   = "\033[0m"
+)

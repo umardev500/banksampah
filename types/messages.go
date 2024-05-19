2f781f867664e8f27d8c03b744ee10bd654f8666
Umar <umar.dev500@gmail.com>
2024-05-19 08:44:28 +0700

    add types

--- types/messages.go
+++ types/messages.go
@@ -1,5 +1,7 @@
 package types
 
+import "fmt"
+
 var WasteType = struct {
 	FailedGetAll  string
 	SuccessGetAll string
@@ -23,3 +25,25 @@ var WasteType = struct {
 // ID
 var InvalidIDMessage string = "Invalid ID"
 var MustUUIDValidError string = "The ID must be a valid UUID"
+
+// Wallet
+var wallet = "wallet"
+var Wallet = struct {
+	FailedGetAll  string
+	SuccessGetAll string
+	FailedDelete  string
+	SuccessDelete string
+	SuccessUpdate string
+	FaildUpdate   string
+	SuccessCreate string
+	FailedCreate  string
+}{
+	FailedGetAll:  fmt.Sprintf("Failed to get all %s", wallet),
+	SuccessGetAll: fmt.Sprintf("Successfully get all %s", wallet),
+	FailedDelete:  fmt.Sprintf("Failed to delete %s", wallet),
+	SuccessDelete: fmt.Sprintf("Successfully delete %s", wallet),
+	SuccessUpdate: fmt.Sprintf("Successfully update %s", wallet),
+	FaildUpdate:   fmt.Sprintf("Failed to update %s", wallet),
+	SuccessCreate: fmt.Sprintf("Successfully create %s", wallet),
+	FailedCreate:  fmt.Sprintf("Failed to create %s", wallet),
+}

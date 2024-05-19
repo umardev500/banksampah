6b7ea1fbdb6ad4fb36c773680bf87466f6d0d4e3
Umar <umar.dev500@gmail.com>
2024-05-19 08:38:52 +0700

    define interface

--- domain/wallet_domain.go
+++ domain/wallet_domain.go
@@ -4,7 +4,12 @@ import (
 	"context"
 
 	"github.com/umardev500/banksampah/domain/model"
+	"github.com/umardev500/banksampah/util"
 )
+
+type WalletUsecase interface {
+	Create(ctx context.Context, payload model.WalletCreateOrUpdateRequest) util.Response
+}
 
 type WalletRepository interface {
 	Create(ctx context.Context, payload model.WalletCreateOrUpdateRequest) error

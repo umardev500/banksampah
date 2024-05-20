db82033f0fce2bc562a110109170fdc74969b017
Umar <umar.dev500@gmail.com>
2024-05-20 09:19:15 +0700

    add check for valid id from struct

--- util/id.go
+++ util/id.go
@@ -1,6 +1,8 @@
 package util
 
 import (
+	"reflect"
+
 	"github.com/gofiber/fiber/v3"
 	"github.com/google/uuid"
 	"github.com/umardev500/banksampah/constant"
@@ -30,4 +32,54 @@ func CheckIDWithResponse(id string) (resp *Response, err error) {
 	}
 
 	return nil, nil
+}
+
+func ChekEntireIDFromStruct(src interface{}) []types.ErrorDetail {
+	var details []types.ErrorDetail
+
+	v := reflect.ValueOf(src)
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		vType := v.Type().Field(i)
+		tag, ok := vType.Tag.Lookup("checkid")
+		if ok && tag != "" {
+			if field.IsZero() {
+				details = append(details, types.ErrorDetail{
+					Code:  string(constant.ErrCodeNameEmpy),
+					Field: tag,
+					Error: "The id must not be empty",
+				})
+				continue
+			}
+
+			_, err := uuid.Parse(field.String())
+			if err != nil {
+				details = append(details, types.ErrorDetail{
+					Code:  string(constant.ErrCodeNameInvalidID),
+					Field: tag,
+					Value: field.String(),
+					Error: "The id must be valid UUID.",
+				})
+				continue
+			}
+		}
+	}
+
+	return details
+}
+
+func ChekEntireIDFromStructWithResponse(src interface{}) (resp *Response, err error) {
+	details := ChekEntireIDFromStruct(src)
+	if len(details) > 0 {
+		return &Response{
+			StatusCode: fiber.StatusBadRequest,
+			Message:    types.InvalidIDMessage,
+			Error: &ResponseError{
+				Code:    constant.ErrCodeNameInvalidIDs,
+				Details: details,
+			},
+		}, types.ErrIDValidation
+	}
+
+	return
 }

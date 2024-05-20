4c0c6e37970408449335a96af02168e400ad5a4f
Umar <umar.dev500@gmail.com>
2024-05-20 10:21:59 +0700

    set default status to unconfirmed

--- database/migration/files/20240518112933_waste_deposits.up.sql
+++ database/migration/files/20240518112933_waste_deposits.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS waste_deposits (
     waste_type_id UUID NOT NULL,
     quantity INTEGER NOT NULL,
     "description" TEXT,
-    "status" VARCHAR(11) CHECK ("status" IN ('confirmed', 'unconfirmed')),
+    "status" VARCHAR(11) DEFAULT 'unconfirmed' CHECK ("status" IN ('confirmed', 'unconfirmed')),
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ DEFAULT NULL,
     deleted_at TIMESTAMPTZ DEFAULT NULL,
